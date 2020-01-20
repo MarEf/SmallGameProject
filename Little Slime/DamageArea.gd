22fc2ed9c5dcd2c2d3b1097bd9bad562a90fcc9d
MarEf <maria.efimova93@gmail.com>
2020-01-20 18:56:36 +0200

    Added a timer to hazards so that the player can reasonably remove the slime from their proximity

--- Little Slime/DamageArea.gd
+++ Little Slime/DamageArea.gd
@@ -3,16 +3,26 @@ extends Area2D
 # var a = 2
 # var b = "text"
 onready var global = get_node("/root/global")
+
+var cooldown_timer = Timer.new()
+var cooldown = false
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	add_child(cooldown_timer)
+	cooldown_timer.connect("timeout", self, "_timeout")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
 
+func _timeout():
+	cooldown = false
+	cooldown_timer.stop()
+
 func _on_DamageArea_body_entered(body):
-	if global.lives>0:
+	if (global.lives>0)&&(!cooldown):
 		global.lives -=1
-	else:
+		cooldown = true
+		cooldown_timer.start(0.5)
+	if(global.lives<=0):
 		print("GAME OVER")
--- Little Slime/TempDamageArea.gd
+++ Little Slime/TempDamageArea.gd
@@ -4,19 +4,28 @@ onready var global = get_node("/root/global")
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
+var cooldown_timer = Timer.new()
+var cooldown = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	add_child(cooldown_timer)
+	cooldown_timer.connect("timeout", self, "_timeout")
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
 
+func _timeout():
+	cooldown = false
+	cooldown_timer.stop()
 
 func _on_TempDamageArea_body_entered(body):
 	print("Damaging collision detected")
-	if global.lives>0:
+	if (global.lives>0)&&(!cooldown):
 		global.lives -=1
-	else:
+		cooldown = true
+		cooldown_timer.start(0.5)
+	if(global.lives<=0):
 		print("GAME OVER")
